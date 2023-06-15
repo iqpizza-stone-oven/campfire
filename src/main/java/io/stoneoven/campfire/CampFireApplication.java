9d1df945fd7a50dab9d788b51ea194204ceff917
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-15 10:49:19 +0900

    :tada: :: start campfire project

---
+++ src/main/java/io/stoneoven/campfire/CampFireApplication.java
@@ -0,0 +1,13 @@
+package io.stoneoven.campfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampFireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampFireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/stoneoven/campfire/CampFireApplicationTests.java
@@ -0,0 +1,13 @@
+package io.stoneoven.campfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampFireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
