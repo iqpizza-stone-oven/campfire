959bd049309cfb8b86df11387996003620116c43
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-17 17:24:34 +0900

    :necktie: :: add register and login logic service

---
+++ src/main/java/io/stoneoven/campfire/modules/account/AccountRepository.java
@@ -0,0 +1,13 @@
+package io.stoneoven.campfire.modules.account;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends CrudRepository<Account, Long> {
+
+    Optional<Account> findAccountByEmail(String email);
+
+}
---
+++ src/main/java/io/stoneoven/campfire/modules/account/AccountService.java
@@ -0,0 +1,40 @@
+package io.stoneoven.campfire.modules.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AccountService extends DefaultOAuth2UserService {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest)
+            throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+        String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+        String profile = oAuth2User.getAttribute("picture");
+
+        Account account = accountRepository.findAccountByEmail(email)
+                .orElse(null);
+        if (account == null) {
+            account = accountRepository.save(Account.builder()
+                    .email(email)
+                    .name(name)
+                    .profileImage(profile)
+                    .registerType(RegisterType.valueOf(provider.toUpperCase()))
+                    .build());
+        }
+
+        return new UserAccount(account, oAuth2User.getAttributes());
+    }
+}
