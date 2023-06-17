d34f540fe4477361b8e9b6b28388fa28c3d54908
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-17 17:19:39 +0900

    :zap: :: create oauth, security user principle wrapper class

---
+++ src/main/java/io/stoneoven/campfire/modules/account/UserAccount.java
@@ -0,0 +1,72 @@
+package io.stoneoven.campfire.modules.account;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class UserAccount implements UserDetails, OAuth2User {
+
+    private final Account account;
+    private Map<String, Object> attributes;
+
+    public UserAccount(Account account) {
+        this.account = account;
+    }
+
+    public UserAccount(Account account, Map<String, Object> attributes) {
+        this.account = account;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getName() {
+        return attributes.get("sub").toString();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+}
