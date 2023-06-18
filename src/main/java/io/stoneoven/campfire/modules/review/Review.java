7e9a3adbf158061b47a6c285f9481f1a8de5467d
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-18 15:51:19 +0900

    :art: :: add simple util methods on review entity

--- src/main/java/io/stoneoven/campfire/modules/review/Review.java
+++ src/main/java/io/stoneoven/campfire/modules/review/Review.java
@@ -1,6 +1,7 @@
 package io.stoneoven.campfire.modules.review;
 
 import io.stoneoven.campfire.modules.account.Account;
+import io.stoneoven.campfire.modules.account.UserAccount;
 import io.stoneoven.campfire.modules.comment.Comment;
 import io.stoneoven.campfire.modules.tag.Tag;
 import jakarta.persistence.*;
@@ -11,12 +12,14 @@ import java.util.Set;
 
 @Entity
 @Getter
+@ToString
 @Builder @AllArgsConstructor @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Review {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ToString.Exclude
     @ManyToOne(optional = false)
     private Account account;
 
@@ -27,10 +30,28 @@ public class Review {
     private String content;
 
     @OneToMany(mappedBy = "review")
+    @ToString.Exclude
     private Set<Comment> comments;
 
     @ManyToMany
+    @ToString.Exclude
     private Set<Tag> tags;
+
+    public void setTags(Set<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public void setWriter(Account writer) {
+        this.account = writer;
+    }
+
+    public boolean isAuthor(UserAccount account) {
+        return this.account.equals(account.getAccount());
+    }
 
     @Override
     public boolean equals(Object o) {
