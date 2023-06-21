1c2274ed29c0b5c67afff91ba7f8d491834e0c7b
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-21 13:49:40 +0900

    :construction: :: prepare to work adapt with mapping in comment-sympathy controller

--- src/main/java/io/stoneoven/campfire/modules/comment/CommentController.java
+++ src/main/java/io/stoneoven/campfire/modules/comment/CommentController.java
@@ -8,10 +8,7 @@ import io.stoneoven.campfire.modules.review.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
