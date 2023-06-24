421c5629f0f60de4b8bde949c454aaa8959cfc76
iqpizza6349 <iqpizza6349@gmail.com>
2023-06-24 15:11:40 +0900

    :zap: :: add default title and message for new typo 'review-forum'

--- src/main/java/io/stoneoven/campfire/modules/notification/event/NotificationEvent.java
+++ src/main/java/io/stoneoven/campfire/modules/notification/event/NotificationEvent.java
@@ -31,6 +31,7 @@ public class NotificationEvent {
             case REVIEW_RECEIVE -> "리뷰 확인을 위해 클릭해주세요.";
             case REVIEW_REQUEST -> "코드 확인을 위해 클릭해주세요.";
             case REVIEW_SELECTED -> "당신의 코드리뷰가 채택되었습니다!";
+            case REVIEW_FORUM -> "포럼 입장을 원하면 클릭해주세요.";
             default -> throw new IllegalStateException("System type must have message");
         };
     }
@@ -39,6 +40,7 @@ public class NotificationEvent {
             case REVIEW_RECEIVE -> "누군가가 리뷰를 남겼습니다.";
             case REVIEW_REQUEST -> "관심있는 주제의 코드 리뷰 요청이 왔습니다.";
             case REVIEW_SELECTED -> "코드 리뷰";
+            case REVIEW_FORUM -> "관심있는 주제의 코드 포럼이 생겼습니다.";
             default -> throw new IllegalStateException("System type must have title");
         };
     }
